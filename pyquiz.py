9ea1f45967729cb10440a5743ddfaf09717829ce
Denis BRANDOLINI <d.brandolini@cineca.it>
2017-12-20 17:20:57 +0100

    Added text (markdown) renderer

--- pyquiz.py
+++ pyquiz.py
@@ -1,6 +1,7 @@
 """An utility to generate shuffled quizzes from a YAML definition"""
 import fire
 import yaml
+from typing import Generator
 from quizzer.loader import QuizzesLoader
 from quizzer.printer import Printer
 
@@ -10,13 +11,41 @@ def _producer(quizfile: str, seed: int = 0, number: int = 1):
         printer = Printer(QuizzesLoader(yamlbuffer=qfile.read()))
     return (printer.to_dict(seed + offset) for offset in range(number))
 
-def to_yaml(quizfile: str, seed: int = 0, number: int = 1):
-    """Return a YAML document with all the printed shuffled quizzes"""
-    return yaml.safe_dump_all(_producer(quizfile, seed, number))
+def render_yaml(gen: Generator):
+    """YAML renderer"""
+    return yaml.safe_dump_all(gen)
+
+def render_text(gen: Generator):
+    """YAML renderer"""
+    def totext(doc):
+        """Convert a single document into text"""
+        txt = '# {}\n[code: {}]\n{}\n\n'.format(doc['title'], doc['seed'], doc['description'])
+        for quiz in doc['quizzes']:
+            txt += '## {} ({}):\n{}\n\n'.format(quiz['id'], 'multi' if quiz['multi'] else 'single', quiz['text'])
+            for opt in quiz['options']:
+                txt += ' - [ ] {}\n'.format(opt)
+            txt += '\n'
+        return txt
+
+    return '\n---\n'.join(totext(adoc) for adoc in gen)
+
+def renderer(quizfile: str, seed: int = 0, number: int = 1, outformat: str = 'YAML'):
+    """Renders the quizfile in the number and format given.
+    
+    Supported formats: TEXT, YAML
+    """
+    if outformat == 'YAML':
+        rend = render_yaml
+    elif outformat == 'TEXT':
+        rend = render_text
+    else:
+        raise Exception("Unknown output format " + outformat)
+    return rend(_producer(quizfile, seed, number))
+
 
 def main():
     """Main program"""
-    fire.Fire(to_yaml)
+    fire.Fire(renderer)
 
 if __name__ == '__main__':
     main()
